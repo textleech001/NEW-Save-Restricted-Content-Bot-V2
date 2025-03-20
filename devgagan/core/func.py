de4f125dc664c459b0e365b3f01f8837006ca12c
KING PROJECT <textleech@gmail.com>
2025-03-20 15:08:12 +0530

    Update func.py

--- devgagan/core/func.py
+++ devgagan/core/func.py
@@ -250,7 +250,7 @@ async def progress_callback(current, total, progress_message):
     f"│ **__Progress:__** {percent:.2f}%\n"
     f"│ **__Uploaded:__** {current_mb:.2f} MB / {total_mb:.2f} MB\n"
     f"╰──────────────────╯\n\n"
-    f"**__Powered by 🍁⚜KING PROJECTS⚜🍁__**"
+    f"**__Powered by ⚜KING PROJECTS⚜__**"
         )
 
         last_update_time = current_time
